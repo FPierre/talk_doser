f2c6991f4dd2fe759142b094ed6c21dd816aafbc
FPierre <pflauder@gmail.com>
2017-05-28 22:26:41 +0200

    Amélioration du nommage des variables

--- doser.py
+++ doser.py
@@ -1,106 +1,92 @@
 import codecs
-import json
 import nltk
 from nltk.corpus import stopwords
 import re
 
-def filter_stopwords(text, stopword_list):
-    # TODO: tokenize instead
-    # words = [w.lower() for w in text]
-    words = re.sub("[^\w]", " ",  text).split()
-    # print(words)
+class Doser:
+    def __init__(self, file_name, people):
+        self.file_name = file_name
+        self.french_stopwords = self.get_stopswords()
+        self.data = {}
+        self.data["words"] = {}
+        self.data["other"] = {}
+        self.data["other"]["sentences"] = []
 
-    # filtered_words = []
-    #
-    # for word in words:
-    #     # Only add words that are not in the French stopwords list, are alphabetic, and are more than 1 character
-    #     if word not in stopword_list and word.isalpha() and len(word) > 1:
-    #         # Add word to filter_words list if it meets the above conditions
-    #         filtered_words.append(word)
-    #
-    # filtered_words.sort()
-    #
-    # print(filtered_words)
-    # return filtered_words
-    return words
+        for person in people:
+            pseudo = person["pseudo"]
+            self.data[pseudo] = {}
+            self.data[pseudo]["sentences"] = []
 
-# get French stopwords from the nltk kit
-def get_stopswords():
-    # Create a list of all French stopwords
-    # raw_stopword_list = stopwords.words('french')
-    stopword_list = stopwords.words('french')
-    # Make to decode the French stopwords as unicode objects rather than ascii
-    # stopword_list = [word.decode('utf8') for word in raw_stopword_list]
+    def filter_stopwords(self, text):
+        # TODO: tokenize instead
+        # words = [w.lower() for w in text]
+        words = re.sub("[^\w]", " ",  text).split()
+        # print(words)
 
-    return stopword_list
+        # filtered_words = []
+        #
+        # for word in words:
+        #     # Only add words that are not in the French stopwords list, are alphabetic, and are more than 1 character
+        #     if word not in stopword_list and word.isalpha() and len(word) > 1:
+        #         # Add word to filter_words list if it meets the above conditions
+        #         filtered_words.append(word)
+        #
+        # filtered_words.sort()
+        #
+        # print(filtered_words)
+        # return filtered_words
+        return words
 
-config_file = open("./secrets.json").read()
-config = json.loads(config_file)
+    def get_stopswords(self):
+        # Create a list of all French stopwords
+        # raw_stopword_list = stopwords.words('french')
+        stopword_list = stopwords.words('french')
+        # Make to decode the French stopwords as unicode objects rather than ascii
+        # stopword_list = [word.decode('utf8') for word in raw_stopword_list]
 
-person_one_pseudo = config["people"][0]["pseudo"]
-person_two_pseudo = config["people"][1]["pseudo"]
+        return stopword_list
 
-data = {}
-data[person_one_pseudo] = {}
-data[person_two_pseudo] = {}
-data["other"] = {}
+    def parse(self):
+        with codecs.open(self.file_name, "r", "utf-8") as f:
+            lines = f.readlines()
+            lines = [x.strip() for x in lines]
 
-data[person_one_pseudo]["sentences"] = []
-data[person_two_pseudo]["sentences"] = []
-data["other"]["sentences"] = []
+            for line in lines:
+                # print(line)
 
-data["words"] = {}
+                if 'str' in line:
+                    break
 
-french_stopwords = get_stopswords()
-# print(french_stopwords)
+                date_time = re.match(r"^(\d{2}\/\d{2}\/\d{4}), (\d{2}:\d{2}) - ", line)
+                date = date_time.group(1)
+                time = date_time.group(2)
+                # print(date)
 
-with codecs.open(config["file"], "r", "utf-8") as f:
-    lines = f.readlines()
-    # Removes whitespace characters
-    lines = [x.strip() for x in lines]
+                line_without_date_time = line[20:]
 
-    for line in lines:
-        # print(line)
+                for pseudo in self.data:
+                    # Expects that a sentence begin with a person pseudo
+                    if line_without_date_time.startswith(pseudo):
+                        pseudo_length = len(pseudo) + 2
+                        line_without_pseudo = line_without_date_time[pseudo_length:]
+                        # data[pseudo]["sentences"].append(line_without_pseudo)
+                        # print(line_without_pseudo)
 
-        if 'str' in line:
-            break
+                        words = self.filter_stopwords(line_without_pseudo)
+                        # print(words)
 
-        t = re.match(r"^(\d{2}\/\d{2}\/\d{4}), (\d{2}:\d{2}) - ", line)
-        date = t.group(1)
-        time = t.group(2)
-        # print(date)
+                        for word in words:
+                            if word in self.data["words"]:
+                                self.data["words"][word] = self.data["words"][word] + 1
+                            else:
+                                self.data["words"][word] = 1
 
-        c = line[20:]
+                        # data[pseudo]["sentences"].append(line_without_pseudo)
 
-        for pseudo in data:
-            # Expects that a sentence begin with a person pseudo
-            if c.startswith(pseudo):
-                z = len(pseudo) + 2
-                c = c[z:]
-                # data[pseudo]["sentences"].append(c)
-                # print(c)
+                        continue
 
-                stps = filter_stopwords(c, french_stopwords)
-                # print(stps)
+    def export(self):
+        sorted_words = [{ k: self.data["words"][k] } for k in sorted(self.data["words"], key = self.data["words"].get, reverse = True)]
+        self.data["words"] = sorted_words
 
-                for stp in stps:
-                    if stp in data["words"]:
-                        data["words"][stp] = data["words"][stp] + 1
-                    else:
-                        data["words"][stp] = 1
-
-                # data[pseudo]["sentences"].append(c)
-
-                continue
-
-s = [{ k: data["words"][k] } for k in sorted(data["words"], key = data["words"].get, reverse = True)]
-
-# for k, v in s:
-#     k, v
-
-data["words"] = s
-
-# print(json.dumps(data["words"]))
-# print(data["words"])
-
-print(json.dumps(data))
+        return self.data
